3b34eff644a90f974363cd420b03fe86dc498359
Lungan Andrei <bibe602@gmail.com>
2021-07-24 13:57:13 +0100

    whileVersion

--- main.py
+++ main.py
@@ -7,22 +7,26 @@ op.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 op.add_argument("--headless")  # don t open a window
 op.add_argument("--no-sandbox")
 op.add_argument("--disable-dev-shm-usage")
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=op)
 count =0
 while True:
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=op)
-    driver.get("https://aerotim.ro/")
 
+    driver.get("https://aerotim.ro/")
     alege_destinatia=driver.find_element_by_xpath("/html/body/div[1]/div/div[1]/div[1]/div[1]/button")
+    time.sleep(2)
     alege_dublin=driver.find_element_by_xpath("/html/body/div[1]/div/div[1]/div[1]/div[3]/div[1]/form/div/ul/li[3]/input")
+    time.sleep(2)
     vote=driver.find_element_by_xpath("/html/body/div[1]/div/div[1]/div[1]/div[3]/div[1]/form/div/p[1]/input")
+    time.sleep(2)
     alege_destinatia.click()
     time.sleep(2)
     alege_dublin.click()
     time.sleep(2)
     vote.click()
-    time.sleep(10)
-    count +=1
+    count += 1
     print(count)
+    time.sleep(10)
+
 
 
 
